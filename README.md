323cfb17e3b1ca836593663ab3cf008ac0f6c227
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2015-07-09 00:10:47 +0400

    Added some basic Angular and ng-route stub with dependencies loaded as external scripts. Can work without Node server, just as simple as single HTML file.

--- README.md
+++ README.md
@@ -18,6 +18,15 @@ Scaffold a new project:
 $ yo angular-stub
 ```
 
+Then do stuff with `index.html`. Out of the box, it has
+
+1. Twitter Bootstrap added for basic styling.
+1. Angular and ng-router added as dependencies.
+1. Some basic routing (non-HTML5 mode) and templating with `text/ng-template`s.
+1. Some basic service to persist data across multiple controllers, keeping in mind that each time a controller is attached to `ng-view` it's being reinstantiated, thus `$scope` reinitialized.
+
+Can be used without Node server running. All dependencies are added via external scripts from CDN.
+
 ## License
 
 MIT
--- generators/app/index.js
+++ generators/app/index.js
@@ -4,54 +4,63 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
-    var done = this.async();
+    prompting: function () {
+        var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the solid ' + chalk.red('AngularStub') + ' generator!'
-    ));
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the solid ' + chalk.red('AngularStub') + ' generator!'
+        ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+        var prompts = [{
+            type: 'confirm',
+            name: 'someOption',
+            message: 'Would you like to enable this option?',
+            default: true
     }];
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
+        this.prompt(prompts, function (props) {
+            this.props = props;
+            // To access props later use this.props.someOption;
 
-      done();
-    }.bind(this));
-  },
-
-  writing: {
-    app: function () {
-      this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
-      );
+            done();
+        }.bind(this));
     },
 
-    projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
-    }
-  },
+    writing: {
+        app: function () {
+            this.fs.copy(
+                this.templatePath('_package.json'),
+                this.destinationPath('package.json')
+            );
+            this.fs.copy(
+                this.templatePath('_bower.json'),
+                this.destinationPath('bower.json')
+            );
+            this.fs.copy(
+                this.templatePath('editorconfig'),
+                this.destinationPath('.editorconfig')
+            );
 
-  install: function () {
-    this.installDependencies();
-  }
+            this.fs.copy(
+                this.templatePath('index.html'),
+                this.destinationPath('index.html')
+            );
+        },
+
+        projectfiles: function () {
+            this.fs.copy(
+                this.templatePath('editorconfig'),
+                this.destinationPath('.editorconfig')
+            );
+            this.fs.copy(
+                this.templatePath('jshintrc'),
+                this.destinationPath('.jshintrc')
+            );
+        }
+    },
+
+    install: function () {
+        this.installDependencies();
+    }
 });
